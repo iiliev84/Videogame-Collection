b3e4445ce3346ddbbf977f829fa29bdcb06a981a
ILIAN ILIEV <iiliev84@gmail.com>
2025-05-30 18:42:13 -0500

    games api created

--- api/platforms.js
+++ api/platforms.js
@@ -3,6 +3,11 @@ const router = express.Router();
 export default router;
 import { createPlatform, getPlatforms, getPlatform, deletePlatform, updatePlatform } from "#db/queries/platforms";
 
+function isValidId(id) {
+  const num = Number(id);
+  return Number.isInteger(num) && num > 0;
+}
+
 router.route("/").get(async (req, res) => {
     const platforms = await getPlatforms();
     res.send(platforms);
